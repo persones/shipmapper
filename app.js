69a0859d37addd0d76f305c15cc4786731dda1a4
person <eyal.person.shahar@gmail.com>
2016-06-01 23:26:20 -0700

    initial commit. not really

---
+++ app.js
@@ -0,0 +1,64 @@
+
+var express = require('express');
+
+var exphbs = require('express-handlebars');
+var net = require('net');
+var exploais = require('exploais');
+var decoder = new exploais.textDecoder();
+
+var app = express();
+var server = require('http').Server(app);
+var io = require('socket.io')(server);
+
+var fs = require('fs');
+
+var config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
+var ais_stream = config.ais_stream;
+
+var inData = "";
+var dataReceived = false;
+
+app.engine('html', exphbs({extname:'html'}));
+app.set('view engine', 'hbs');
+app.use('/static', express.static(__dirname + '/'));
+app.use('/public', express.static(__dirname + '/public/'));
+app.use('/socket.io', express.static(__dirname + '/socket.io/lib/'));
+
+
+app.get('/', function(req, res){
+  res.render('shipmapper.html');
+});
+server.listen(1337);
+
+setInterval(function() {
+  if (!dataReceived) {
+    console.log("idle too long. trying to reconnect");
+    try {
+      aisStream.destroy();
+      aisStream.connect(ais_stream.port, ais_stream.url, function() {
+        console.log('Connected to bitway');
+      }); 
+    }
+    catch(err) {
+      console.error(err);
+    }
+  }
+  dataReceived = false;
+}, 60000);
+
+var aisStream = new net.Socket();
+console.log("connectig to bitway");
+aisStream.connect(ais_stream.port,ais_stream.url, function() {
+  console.log('Connected to bitway');
+});
+
+aisStream.on('data', function(data) {
+  dataReceived = true;
+  var splitData = data.toString().split('\n');
+  for (var i = 0; i < splitData.length; i++) {
+    if (splitData[i].length === 0) {
+      continue;
+    }
+    io.emit('ship_data', decoder.decode(splitData[i]));
+  }
+});
